7f093d51166ba9bbf29b823bb61e6338f4dca7a6
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2022-06-14 18:26:27 -0300

    creating retrofit service interface

---
+++ app/src/main/java/com/rodrigo/movies/data/api/TMDBService.kt
@@ -0,0 +1,21 @@
+package com.rodrigo.movies.data.api
+
+import com.rodrigo.movies.data.artist.ArtistList
+import com.rodrigo.movies.data.movie.MovieList
+import com.rodrigo.movies.data.tvshow.TvShowsList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TMDBService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(@Query("api_key") apiKey: String): Response<MovieList>
+
+    @GET("tv/popular")
+    suspend fun getPopularTvShows(@Query("api_key") apiKey: String): Response<TvShowsList>
+
+    @GET("person/popular")
+    suspend fun getPopularArtists(@Query("api_key") apiKey: String): Response<ArtistList>
+
+}
